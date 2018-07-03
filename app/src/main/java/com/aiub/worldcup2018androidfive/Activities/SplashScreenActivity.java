3a920f9c69f89c4bb2e3bff3d87aa8235a1b62c2
nazran91 <nazranacc@gmail.com>
2018-07-03 21:09:06 +0600

    3 june commit

---
+++ app/src/main/java/com/aiub/worldcup2018androidfive/Activities/SplashScreenActivity.java
@@ -0,0 +1,70 @@
+package com.aiub.worldcup2018androidfive.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.aiub.worldcup2018androidfive.R;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final String TAG = SplashScreenActivity.class.getSimpleName();
+    private String API_URL = "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_scren);
+
+        getJSONData();
+    }
+
+    private void getJSONData() {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(SplashScreenActivity.this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e(TAG, response.toString());
+
+                try {
+                    JSONArray stadiums = response.getJSONArray("stadiums");
+
+                    for (int i = 0; i < stadiums.length(); i++) {
+                        JSONObject jsonObject = stadiums.getJSONObject(i);
+
+                        Log.e(TAG, ""+jsonObject.getInt("id"));
+                        Log.e(TAG, ""+jsonObject.getString("name"));
+                        Log.e(TAG, ""+jsonObject.getString("city"));
+                        Log.e(TAG, ""+jsonObject.getDouble("lat"));
+                        Log.e(TAG, ""+jsonObject.getDouble("lng"));
+                        Log.e(TAG, ""+jsonObject.getString("image"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+            }
+        }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+    }
+}
